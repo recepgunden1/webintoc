534e2268b9216f0f722f8e76e6420cc9c0d9a814
recep <recepgunden35@gmail.com>
2024-04-14 15:21:56 +0300

    moduller kuruldu

--- webintoc.py
+++ webintoc.py
@@ -1,4 +1,6 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
