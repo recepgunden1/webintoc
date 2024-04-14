2c546560968ecce97c342595a0d3acd19fcf2631
recep <recepgunden35@gmail.com>
2024-04-14 16:43:20 +0300

    form oluşturuldu

--- webintoc.py
+++ webintoc.py
@@ -7,6 +7,17 @@ app = Flask(__name__)
 con = sql.connect("webintoc.db")
 cursor = con.cursor()
 
+# Kullanıcı kayıt formu
+class RegisterForm(Form):
+    name = StringField("İsim Soyisim",validators=[validators.Length(min = 4, max = 25)])
+    username = StringField("Kullanıcı Adı",validators=[validators.Length(min = 4, max = 15)])
+    email = StringField("Email adresi",validators=[validators.Email(message="Lütfen geçerli bir email adresi giriniz.")])
+    password = PasswordField("Parola",validators=[
+        validators.DataRequired(message="Lütfen bir parola belirleyiniz."),
+        validators.EqualTo(fieldname="confirm",message="Parolanız uyuşmuyor")
+    ])
+    confirm = PasswordField("Parola Tekrar")
+
 @app.route("/")
 def index():
     return render_template("index.html")
