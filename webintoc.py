fb22ec34a0c064ade4436c36b32e6dbe7663fb76
recep <recepgunden35@gmail.com>
2024-04-20 11:24:43 +0300

    login template i oluşturuldu

--- webintoc.py
+++ webintoc.py
@@ -6,7 +6,9 @@ import sqlite3 as sql
 app = Flask(__name__)
 app.secret_key = "webintoc"
 
-# Kullanıcı kayıt formu
+#######################
+#Kullanıcı kayıt formu
+#######################
 class RegisterForm(Form):
     name = StringField("İsim Soyisim",validators=[validators.Length(min = 4, max = 25)])
     username = StringField("Kullanıcı Adı",validators=[validators.Length(min = 4, max = 15)])
@@ -17,14 +19,30 @@ class RegisterForm(Form):
     ])
     confirm = PasswordField("Parola Tekrar")
 
+#######################
+#Kullanıcı giriş formu
+#######################
+class LoginForm(Form):
+    username = StringField("Kullanıcı Adı")
+    password = PasswordField("Parola")
+
+#######################
+#Ana Sayfa
+#######################
 @app.route("/")
 def index():
     return render_template("index.html")
 
+#######################
+#Hakkımızda
+#######################
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+#######################
+#Kayıt Ol
+#######################
 @app.route("/register",methods=["GET","POST"])
 def register():
     form = RegisterForm(request.form)
@@ -42,9 +60,17 @@ def register():
             con.commit()
             
         flash("Başarıyla kayıt oldunuz!","success")
-        return redirect(url_for("index"))
+        return redirect(url_for("login"))
     else:
         return render_template("register.html",form = form)
+
+#######################
+#Giriş Yap
+#######################
+@app.route("/login", methods = ["GET","POST"])
+def login():
+    form = LoginForm(request.form)
+    return render_template("login.html",form = form)
 
 if __name__ == "__main__":
     app.run(debug=True)
